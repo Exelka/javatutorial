eba48b6dcf757a8709cd16fc5999e6c75dcd350f
acer <swag@swag.pl>
2023-08-29 18:26:00 +0200

    initial commit

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        System.out.println("SWAGWSWADAWDHKWLHAD");
+        ;
+    }
+}
